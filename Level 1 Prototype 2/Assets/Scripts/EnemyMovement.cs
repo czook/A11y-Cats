fb63018dc32ddc8b721acb24a5d5851556e4a4fb
Camdyn Zook <czook@iastate.edu>
2022-02-15 22:04:41 -0600

    initial commits

---
+++ Level 1 Prototype 2/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public int movementSpeed;
+    public Transform[] moveSpots;
+    private int currentSpot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSpot = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[currentSpot + 1].position, movementSpeed * Time.deltaTime);
+    }
+
+}
---
+++ Level 1 Prototype 2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontalInput = 0;
+    private float verticalInput = 0;
+    public int movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        GetPlayerInput();
+        MovePlayer();
+    }
+
+    private void GetPlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 directionVector = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(directionVector.normalized * Time.deltaTime * movementSpeed);
+    }
+}
